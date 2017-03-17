9d91458e3880d8b47520ce145ae7bccb7a60ea44
JefftyLee <Pliskin840331@hotmail.com>
2017-03-17 16:11:14 +0800

    Finish the changes for auto build

--- build_full/buildall.py
+++ build_full/buildall.py
@@ -1,7 +1,7 @@
 import os
 
-os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=dev" "workdir=E:/simcity/trunk/client" "target=Android_dev" "server_name=Dev" "IsBuildData=1" "commit_versioninfo=0" ../project/simcity_main_build.bld')
-os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=release" "workdir=E:/simcity/trunk/client" "target=Android_prod_qa" "server_name=Prod_qa" "IsBuildData=0" "commit_versioninfo=0" ../project/simcity_main_build.bld')
-os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=release" "workdir=E:/simcity/trunk/client" "target=Android_prod" "server_name=Prod" "IsBuildData=0" "commit_versioninfo=0" ../project/simcity_main_build.bld')
-os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=qa" "workdir=E:/simcity/trunk/client" "target=Android_latest" "server_name=Kingsoft" "IsBuildData=0" "commit_versioninfo=1" ../project/simcity_main_build.bld')
+os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=dev" "workdir=E:/simcity/trunk/client" "target=Android_dev" "server_name=Dev" "IsBuildData=1" "commit_versioninfo=0" ../project/ibcs_main_build.bld')
+os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=release" "workdir=E:/simcity/trunk/client" "target=Android_prod_qa" "server_name=Prod_qa" "IsBuildData=0" "commit_versioninfo=0" ../project/ibcs_main_build.bld')
+os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=release" "workdir=E:/simcity/trunk/client" "target=Android_prod" "server_name=Prod" "IsBuildData=0" "commit_versioninfo=0" ../project/ibcs_main_build.bld')
+os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=qa" "workdir=E:/simcity/trunk/client" "target=Android_latest" "server_name=Kingsoft" "IsBuildData=0" "commit_versioninfo=1" ../project/ibcs_main_build.bld')
 os._exit(0)
