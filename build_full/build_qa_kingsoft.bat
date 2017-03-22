774998a283e85b7087115e053e137df9888ba86e
JefftyLee <Pliskin840331@hotmail.com>
2017-03-22 15:44:52 +0800

    update version info

--- build_full/build_qa_kingsoft.bat
+++ build_full/build_qa_kingsoft.bat
@@ -1,3 +1,3 @@
-E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=qa" "workdir=E:/simcity/trunk/client" "target=Android_latest" "server_name=Kingsoft" "IsBuildData=1" "commit_versioninfo=1" "compile_code=1" ../project/ibcs_main_build.bld
+E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=qa" "workdir=E:/simcity/trunk/client" "target=Android_latest" "server_name=Kingsoft" "IsBuildData=0" "commit_versioninfo=1" "compile_code=1" ../project/ibcs_main_build.bld
 
 pause
