1a81224083078604926a853f494f658592ce43d7
JefftyLee <Pliskin840331@hotmail.com>
2017-04-06 19:35:20 +0800

    Add fakepayment build

---
+++ build_full/build_fakepayment_kingsoft.bat
@@ -0,0 +1,3 @@
+E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=fakepayment" "workdir=E:/simcity/trunk/client" "target=Android_latest" "server_name=Kingsoft" "IsBuildData=0" "commit_versioninfo=1" "compile_code=1" "FakePayment=1" ../project/ibcs_main_build.bld
+
+pause
