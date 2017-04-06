9de5c75c3581af09f4c47d9976573491662d7c49
JefftyLee <Pliskin840331@hotmail.com>
2017-04-06 15:10:20 +0800

    Add fakepayment build

--- build_full/build_dev_Dev.bat
+++ build_full/build_dev_Dev.bat
@@ -1,3 +1,3 @@
-E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=dev" "workdir=E:/simcity/trunk/client" "target=Android_dev" "server_name=Dev" "IsBuildData=1" "commit_versioninfo=1" "compile_code=1" ../project/ibcs_main_build.bld
+E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=dev" "workdir=E:/simcity/trunk/client" "target=Android_dev" "server_name=Dev" "IsBuildData=1" "commit_versioninfo=1" "compile_code=1" "FakePayment=0" ../project/ibcs_main_build.bld
 
 pause
--- build_full/build_prod_release.bat
+++ build_full/build_prod_release.bat
@@ -1,3 +1,3 @@
-E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=release" "workdir=E:/simcity/trunk/client" "target=Android_prod" "server_name=Prod" "IsBuildData=1" "commit_versioninfo=1" "compile_code=1" ../project/ibcs_main_build.bld
+E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=release" "workdir=E:/simcity/trunk/client" "target=Android_prod" "server_name=Prod" "IsBuildData=1" "commit_versioninfo=1" "compile_code=1" "FakePayment=0" ../project/ibcs_main_build.bld
 
 pause
