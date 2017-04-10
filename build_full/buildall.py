58927392c809c4b23c7de6d4843c99b216756fa4
JefftyLee <Pliskin840331@hotmail.com>
2017-04-10 18:17:31 +0800

    update version info

--- build_full/buildall.py
+++ build_full/buildall.py
@@ -1,5 +1,5 @@
 import os
-os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=fakePayment" "workdir=E:/simcity/trunk/client" "target=Android_latest" "server_name=Kingsoft" "IsBuildData=1" "commit_versioninfo=0" "compile_code=1" "FakePayment=1" ../project/ibcs_main_build.bld')
+os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=fakePayment" "workdir=E:/simcity/trunk/client" "target=Android_latest" "server_name=Kingsoft" "IsBuildData=0" "commit_versioninfo=0" "compile_code=1" "FakePayment=1" ../project/ibcs_main_build.bld')
 os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=release" "workdir=E:/simcity/trunk/client" "target=Android_prod" "server_name=Prod" "IsBuildData=0" "commit_versioninfo=0" "compile_code=1" "FakePayment=0" ../project/ibcs_main_build.bld')
 os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=qa" "workdir=E:/simcity/trunk/client" "target=Android_latest" "server_name=Kingsoft" "IsBuildData=0" "commit_versioninfo=0" "compile_code=1" "FakePayment=0" ../project/ibcs_main_build.bld')
 os.system('E:/simcity/VisBuildPro8/VisBuildCmd.exe "build_type=qa" "workdir=E:/simcity/trunk/client" "target=Android_prod_qa" "server_name=Prod" "IsBuildData=0" "commit_versioninfo=0" "compile_code=0" "FakePayment=0" ../project/ibcs_main_build.bld')
